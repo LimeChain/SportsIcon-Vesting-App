1483f4538a3ce5ba7385317241e74881317c2031
LuboslavBurdenski <luboslav990226@gmail.com>
2021-11-10 16:17:37 +0200

    change testnet network to `Rinkeby`

--- src/config/config.js
+++ src/config/config.js
@@ -2,12 +2,12 @@ const config = {
     "testnet": {
         "network": {
             "symbol": "ETH",
-            "name": "Ethereum Ropsten",
-            "network": "ropsten",
-            "chain_id": 3,
-            "network_id": 3,
+            "name": "Ethereum Rinkeby",
+            "network": "rinkeby",
+            "chain_id": 4,
+            "network_id": 4,
             "rpc_url": `https://ropsten.infura.io/v3/${process.env.REACT_APP_INFURA_ID}`,
-            "explorer_url": "https://ropsten.etherscan.io/tx/"
+            "explorer_url": "https://rinkeby.etherscan.io/tx/"
         },
         "contract": "0x67E5319DF1E577F482446d07cC0C23819890E6ee",
         "token": "0x21E5A30D8A325c24920570bEF1F30E48EB1bF0Ba"
