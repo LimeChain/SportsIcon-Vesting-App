7fba1902760aeb2c1880a9b7efdfb59d8c0694ce
LuboslavBurdenski <luboslav990226@gmail.com>
2021-11-22 18:08:23 +0200

    update abi and sdk

--- src/config/config.js
+++ src/config/config.js
@@ -9,8 +9,8 @@ const config = {
             "rpc_url": `https://rinkeby.infura.io/v3/${process.env.REACT_APP_INFURA_ID}`,
             "explorer_url": "https://rinkeby.etherscan.io/tx/"
         },
-        "contracts": ["0x4b928BE2771A30bE8FAed062195150e1837E1CcD", "0xB3f1000dC34C0B59a148f040fe536d9DABDe6c8C", "0x19E6378cb7B023FC25d8e27C4b036c3E48581Bfb"],
-        "token": "0xF6A74f24123171Ddf469B063f8230E351Ef7d50F"
+        "contracts": ["0x2B837182c18502cebD0420eE75B8FA1FFfC1E9EC", "0x30a08e26c114Be3F3cB4a60A8BD83E8E2B1969aa", "0x5a5364C70eFc59569D85d412E363772A9C49DcfA"],
+        "token": "0xC8479c296216C5e581058d06DF0bdb3b411c6119"
     },
     "mainnet": {
         "network": {
