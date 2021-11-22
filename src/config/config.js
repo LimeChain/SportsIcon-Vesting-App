8873b1799b5e01520307079d84d2592af383d7fb
LuboslavBurdenski <luboslav990226@gmail.com>
2021-11-22 18:23:01 +0200

    change addresses of contracts

--- src/config/config.js
+++ src/config/config.js
@@ -9,8 +9,8 @@ const config = {
             "rpc_url": `https://rinkeby.infura.io/v3/${process.env.REACT_APP_INFURA_ID}`,
             "explorer_url": "https://rinkeby.etherscan.io/tx/"
         },
-        "contracts": ["0x2B837182c18502cebD0420eE75B8FA1FFfC1E9EC", "0x30a08e26c114Be3F3cB4a60A8BD83E8E2B1969aa", "0x5a5364C70eFc59569D85d412E363772A9C49DcfA"],
-        "token": "0xC8479c296216C5e581058d06DF0bdb3b411c6119"
+        "contracts": ["0x6Dd5a10C0A39838f8fDA2D40156aba01f17F7F95", "0x5C34c083C251AeD727d49b5Ab7ae32eE65e0Aad8", "0xBc17D3E15c6fDBA9BfE9689F370a284F1ecf5278"],
+        "token": "0xBf2036D15372ebbDB3f3884Edd097E21DD87bB62"
     },
     "mainnet": {
         "network": {
